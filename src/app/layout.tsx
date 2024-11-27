d38f81068b60361ea20dbff41d1779aad592a684
Ralph Rosael <ralphrosael27@gmail.com>
2024-11-27 22:39:58 +0800

    feat: Added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { Metadata } from 'next';
 
 import { ThemeProvider } from '@/components/theme-provider';
@@ -7,8 +8,8 @@ import '../styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'RepoTree - ASCII Tree Generator',
-  description:
-    'RepoTree is a web app for generating an ASCII tree from a GitHub URL.',
+  description: 'RepoTree is a web app for generating an ASCII tree from a GitHub URL.',
+  authors: [{ name: 'Ralph Rosael' }],
 };
 
 export default function RootLayout({
@@ -18,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7PSC7S1M55"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7PSC7S1M55');
+          `}
+        </Script>
+      </head>
       <body className="antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
