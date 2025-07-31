2529ace7b95b7bba5a85f03ff62c2aff4a8caf29
Ralph Rosael <ralphrosael27@gmail.com>
2025-07-31 20:50:08 +0800

    Remove Google Analytics and Search Console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import type { Metadata } from 'next';
 
 import { ThemeProvider } from '@/components/theme-provider';
@@ -10,10 +9,6 @@ export const metadata: Metadata = {
   title: 'RepoTree - ASCII Tree Generator',
   description: 'RepoTree is a web app for generating an ASCII tree from a GitHub or GitLab URL.',
   authors: [{ name: 'Ralph Rosael' }],
-  verification: {
-    // Google Search Console
-    google: 'T1eSo0As2QM6eFiFVR-rfDwCxNa_oMVNVgoSDqZNk4U',
-  },
 };
 
 export default function RootLayout({
@@ -23,21 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* Google Tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-7PSC7S1M55"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-7PSC7S1M55');
-          `}
-        </Script>
-      </head>
       <body className="antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
